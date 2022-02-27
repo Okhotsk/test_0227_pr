3c723c4acbe14b4921178d479fac51361344826d
OkhotskSK <hshift1212.me00@g2.nctu.edu.tw>
2022-02-27 16:09:20 +0800

    open example.txt for read/write

--- main.c
+++ main.c
@@ -4,6 +4,15 @@
 
 int main()
 {
+    FILE *p_src_file = NULL;
+
+    // open file for read/write
+    p_src_file = fopen("example.txt", "rb+");
+    if(p_src_file == NULL)
+    {
+       perror("fopen error\n");
+    }
+
     printf("hello world");
     return 0;
 }
